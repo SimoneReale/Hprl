989bcd21a62cb67c36113aa4a9aed91b419e8794
Simone Reale <srsimonereale98@hotmail.it>
2022-12-24 18:42:31 +0100

    aggiunto supporto default font e modificato funzioni oper fare resize delle texture

--- VulkanCodeExample/src/Application.cpp
+++ VulkanCodeExample/src/Application.cpp
@@ -1,20 +1,15 @@
 #define HPRL_IMAGE_WRITER_IMPLEMENTATION
 #define HPRL_IMPLEMENTATION
+#define HPRL_DEFAULT_FONT
 #include "hprl.h"
 #include <chrono>
 #include <cstdio>
 #include <windows.h>
-#include "default_font.h"
 
 #pragma execution_character_set( "utf-8" )
 
-//#define STB_IMAGE_WRITE_IMPLEMENTATION
-//#include "stb_image_write.h"
 typedef std::chrono::high_resolution_clock Clock;
 
-
-
-using json = nlohmann::json;
 
 //DA FARE: SERIALIZATION (?)
 //CACHE DELLE TEXTURE
@@ -39,12 +34,29 @@ int main() {
 
     SetConsoleOutputCP(65001);
 
-    
-    std::u8string in_big = u8R"({"lines":[{"content":{"ops":[{"attributes":{"size":"60px","color":"#e60000"},"insert":"ARTIFICIAL FUNZIONA"},{"attributes":{"size":"60px"},"insert":" "},{"attributes":{"size":"60px","color":"#9933ff"},"insert":"NORMAL"}]},"line_attributes":{}},{"content":{"ops":[{"attributes":{"size":"60px","color":"#e60000","bold":true},"insert":"ARTIFICIAL BOLD "},{"attributes":{"size":"60px","color":"#9933ff"},"insert":"YES"}]},"line_attributes":{}},{"content":{"ops":[{"attributes":{"size":"60px","color":"#e60000","italic":true},"insert":"ARTIFICIAL BOLD"},{"attributes":{"size":"60px","color":"#e60000"},"insert":" "},{"attributes":{"size":"60px","color":"#9933ff"},"insert":"NOT"}]},"line_attributes":{}}],"dimensions":{"width":"1728px","height":"750px"}})";
+    std::u8string in_big = u8R"({"lines":[{"content":{"ops":[{"attributes":{"size":"40px","bold":true},"insert":"Giovanni"},{"attributes":{"size":"40px"},"insert":" è "},{"attributes":{"underline":true,"size":"40px","color":"#e60000","italic":true},"insert":"stronzo"}]},"line_attributes":{}},{"content":{"ops":[{"attributes":{"size":"40px","color":"#9933ff","strike":true},"insert":"Giulia"},{"attributes":{"size":"40px"},"insert":" "},{"attributes":{"italic":true,"size":"40px","bold":true},"insert":"anche"}]},"line_attributes":{"align":"center"}}],"dimensions":{"width":"400px","height":"400px"}})";
+    std::vector<float> myHeights{40};
+    FontFamilyManager tm = FontFamilyManager(myHeights, 80);
 
-    std::vector<float> myHeights{20};
+    TextCreator x{ 1000, 1000 };
 
-    FontFaceDescription *f_normal = new FontFaceDescription("c:/windows/fonts/arial.ttf", 0, "NORMAL" );
+    x
+      << 30 << Color{"#e60000"}
+      << "Questo è italic" << italic << lst_ord << el 
+      << "Questo è bold underlined" << bold << underline << 40 << Color{ "#e600e6" } << lst_bul << el
+      << "Questa è una lista" << align_c << Color{ "#e600e6" } << lst_ord;
+
+    Texture text;
+    std::size_t hash = tm.parseFromEditor(in_big, text);
+  
+    auto t1 = Clock::now();
+
+    ImageWriter::createPng("test_DEFAULT_FONT.png", text);
+
+    auto t2 = Clock::now();
+
+
+    /*FontFaceDescription *f_normal = new FontFaceDescription("c:/windows/fonts/arial.ttf", 0, "NORMAL" );
     FontFaceDescription *f_bold = new FontFaceDescription("c:/windows/fonts/arialbd.ttf", 0, "BOLD");
     FontFaceDescription* f_italic = new FontFaceDescription("c:/windows/fonts/ariali.ttf", 0, "ITALIC");
     FontFaceDescription *f_bolditalic = new FontFaceDescription("c:/windows/fonts/arialbi.ttf", 0, "BOLDITALIC");
@@ -52,27 +64,8 @@ int main() {
     std::vector<FontFaceDescription> descr{};
 
     FontFamilyInitializer bas_descr = FontFamilyInitializer(f_normal, f_bold, f_italic, f_bolditalic, descr);
-    FontFamilyManager tm = FontFamilyManager(bas_descr, 32, 95, myHeights, 80);
+    FontFamilyManager tm = FontFamilyManager(bas_descr, 32, 95, myHeights, 80);*/
 
-    
-
-    /*Creator x{ 1000, 1000 };
-
-    x
-      << 30 << Color{"#e60000"}
-      << "Questo è italic" << italic << lst_ord << el 
-      << "Questo è bold underlined" << bold << underline << 40 << Color{ "#e600e6" } << lst_bul << el
-      << "Questa è una lista" << align_c << Color{ "#e600e6" } << lst_ord;*/
-
-
-    Texture text;
-    std::size_t hash = tm.parseFromEditor(in_big, text);
-  
-    auto t1 = Clock::now();
-
-    ImageWriter::createPng("test_con_modifiche.png", text);
-
-    auto t2 = Clock::now();
 
     std::cout << "\n\nDelta time t2-t1: "
         << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()
